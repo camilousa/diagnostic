507d2124edcdbab5714249d75b765d87f6d1a2d6
camilousa <83974882+camilousa@users.noreply.github.com>
2023-06-29 14:53:01 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="diagnostic",
+
+    description="Example project structure.",
+
+    author="Camilo Rodriguez",
+
+    packages=find_packages(),
+
+    long_description=read('README.md'),
+)
